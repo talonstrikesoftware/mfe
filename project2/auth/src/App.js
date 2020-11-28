63cd45b10156c2988f370df7c6e9e123a6999703
Mark Astin <redwolf89@gmail.com>
2020-11-28 09:47:10 -0500

    poked auth so it built

--- project2/auth/src/App.js
+++ project2/auth/src/App.js
@@ -11,16 +11,17 @@ import Signup from "./components/Signup";
 const generateClassName = createGenerateClassName({
   productionPrefix: "au",
 });
+
 export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin" >
+            <Route path="/auth/signin">
               <Signin onSignIn={onSignIn} />
             </Route>
-            <Route path="/auth/signup" >
+            <Route path="/auth/signup">
               <Signup onSignIn={onSignIn} />
             </Route>
           </Switch>
